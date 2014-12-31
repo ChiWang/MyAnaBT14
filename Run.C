b435cfdcf44143656673bd7202546c144050df3b
Chi Wang <chiwang@mail.ustc.edu.cn>
2014-12-31 15:56:27 +0800

    modified:   MyCombine.C modified:   Run.C

--- Run.C
+++ Run.C
@@ -25,32 +25,6 @@ enum FileType{
 };
 
 //-------------------------------------------------------------------
-int Load(short type, TString InFN="NO")
-{
-  TString f = "NO";
-  switch (type){
-    case electron:
-      f = "./AMS/SPS/Combine_run_1416070809_ANC_366.root";
-      break;
-    case muon:
-      f = "./AMS/SPS/Combine_run_1416338929_ANC_476.root";
-      break;
-    case photon:
-      f = "./AMS/SPS/Combine_run_1416276173_ANC_451.root";
-      break;
-  }
-  return Conf::LoadInput(f);
-}
-
-//-------------------------------------------------------------------
-void test()
-{
-  AMS::Performance::Clusters();
-  AMS::Alignment::SingleStrack_S_Side();
-  //Tracking::Plots(nEvt);
-}
-
-//-------------------------------------------------------------------
 void CombineAMSSPS()
 {
   ifstream f_0("./AMS/GoodRunList_Side0.log");
@@ -62,8 +36,8 @@ void CombineAMSSPS()
   while((!f_0.eof()) && (!f_1.eof())){
     f_0.getline(name0,256);
     f_1.getline(name1,256);
+    cout<<"\nProcessing\t"<<name0<<"\t"<<name1;//<<endl;
     long entries = CombineData::AMSSPS(name0,name1);
-    cout<<"\nProcessing\t"<<name0<<"\t"<<name1;
     if(entries > 0){
       logOut<<name0<<"\t"<<name1<<"\t"<<entries<<endl;
     }
@@ -87,7 +61,7 @@ void CombineAll()
     if(dampe_file == "" || ams_file =="" || anc_file ==""){
       continue;
     }else{
-      cout<<"\nProcessing\t"<<dampe_file<<"\t"<<ams_file<<"\t"<<anc_file<<endl;
+      cout<<"\nProcessing\t"<<dampe_file<<"\t"<<ams_file<<"\t"<<anc_file;//<<endl;
     }
     long nEvt = CombineData::DAMPE_AMS_ANC(dampe_file,ams_file,anc_file);
     if(nEvt > 0){
